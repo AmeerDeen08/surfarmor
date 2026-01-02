6c69927f93b63ea03e2b9836bf81a358d81a7a35
Ameer <ameerdeen592@gmail.com>
2026-01-02 19:54:21 +0530

    Initial Commit - SurfArmor

---
+++ SurfArmor/entrypoints/popup/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <div className="w-80 h-100 p-4 bg-red-500 text-white font-bold">
+      Tailwind is working 🚀
+    </div>
+    </>
+  );
+}
+
+export default App;
